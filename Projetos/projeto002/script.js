169075b08110013562dc1e6803e03ae78e1fcecc
GuilhermeRippel <guilhermerippelmoraes@gmail.com>
2023-09-21 23:36:26 -0300

    Atualizando projeto Sistema de Listas

--- Projetos/projeto002/script.js
+++ Projetos/projeto002/script.js
@@ -1,19 +1,45 @@
-function adicionar(){
-    let tarefa = document.getElementById('tarefa')
-    let texto = tarefa.value
+let textoDigitado = document.getElementById('textoDigitado');
+let res = document.getElementById('res');
 
-    if(texto.trim() !== ''){
-        let listaTarefas = document.getElementById('listaTarefas')
-        let novoItem = document.createElement('li')
-        novoItem.textContent = texto
-        novoItem.style.background = 'grey'
-        novoItem.style.borderRadius = '5px'
-        listaTarefas.appendChild(novoItem)
-        tarefa.value = ''
+function adicionar() {
+    let texto = textoDigitado.value;
+    let edit = 'Editar';
+    let check = '';
+    if (texto.trim() === '') {
+        window.alert('Por favor, digite um valor de entrada');
+        return;
     }
+
+    let listaNova = document.createElement('li');
+    let editar = document.createElement('button');
+    let checar = document.createElement('button')
+    checar.className = 'fa-solid fa-check';
+    editar.className = 'editar';
+    checar.textContent = check;
+    editar.textContent = edit;
+    listaNova.textContent = texto;
+
+    checar.addEventListener('click', function(){
+        listaNova.innerHTML = `<s>${texto}</s>`;
+        listaNova.appendChild(editar);
+    })
+
+    editar.addEventListener('click', function () {
+        let novoTexto = prompt('Digite o novo texto:');
+        if (novoTexto !== null) {
+            listaNova.textContent = novoTexto;
+            listaNova.appendChild(checar);
+            listaNova.appendChild(editar);
+        }
+    });
+    listaNova.appendChild(checar);
+    listaNova.appendChild(editar);
+    res.appendChild(listaNova);
+
+    textoDigitado.value = '';
 }
 
+
 function limpar(){
-    let listaTarefas = document.getElementById('listaTarefas')
-    listaTarefas.innerHTML = ''
+    res.innerHTML = ''
 }

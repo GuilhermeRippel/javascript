ecb98f2eed6e26ef2f151d249151bc4d26475d76
GuilhermeRippel <guilhermerippelmoraes@gmail.com>
2023-09-22 23:21:04 -0300

    Projeto lista 2.0

---
+++ Projetos/projeto003/script.js
@@ -0,0 +1,33 @@
+
+let res = document.getElementById('res');
+
+function adicionar(){
+    let texto = document.getElementById('texto');
+    let novoTexto = texto.value;
+    let editarTexto = 'EDITAR'
+    let listaNova = document.createElement('li')
+    let editar = document.createElement('button')
+    listaNova.textContent = novoTexto
+    editar.textContent = editarTexto
+    editar.className = 'editar'
+    listaNova.className = 'listaNova'
+
+    editar.addEventListener('click', function(){
+        let novoTexto = prompt('Digite o novo texto')
+        if(novoTexto !== null){
+            listaNova.textContent = novoTexto
+            listaNova.appendChild(editar);
+        }
+    })
+
+    listaNova.appendChild(editar)
+    res.appendChild(listaNova,)
+    texto.value = ''
+}
+
+
+
+
+function limpar(){
+    res.innerHTML = ''
+}
